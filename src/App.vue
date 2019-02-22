761fad2bbfccf2b0902ca46d8d3254ce76d9247c
Brian Watkins <bwatkins@pivotal.io>
2019-02-22 18:08:46 -0500

    Added SuperList component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Here is my app.</h1>
+    <SuperList />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import SuperList from './components/SuperList.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    SuperList,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/SuperList.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import Vue from 'vue'
+export default class SuperList extends Vue {
+
+}
+</script>
+
+<template>
+    <div>Here is a list</div>
+</template>
